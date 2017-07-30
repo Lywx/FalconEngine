8c680270347d804c37c3f48828c0282d5cb52508
Wuxiang Lin <linwx_xin@hotmail.com>
2017-07-30 19:52:08 +0800

    Updated ignored files.

---
+++ src/FalconEngine/Content/Pipeline/AssetMain.cpp
@@ -0,0 +1,34 @@
+#include <FalconEngine/Content/AssetTool.h>
+#include <FalconEngine/Content/AssetProcessor.h>
+#include <FalconEngine/Graphics/Renderer/Platform/OpenGL/OGLShader.h>
+#include <FalconEngine/Graphics/Renderer/Shader/Shader.h>
+
+using namespace std;
+using namespace FalconEngine;
+
+void
+BakeAssets()
+{
+    auto assetProcessor = AssetProcessor();
+
+    // Fonts
+    // assetProcessor.BakeFont("Content/Font/LuciadaConsoleDistanceField.fnt");
+    // assetProcessor.BakeFont("Content/Font/NSimSunDistanceField.fnt");
+
+    // Models
+    // assetProcessor.BakeModel("Content/Model/Engine/Axe.dae");
+    // assetProcessor.BakeModel("Content/Model/Engine/Box.dae");
+
+    // Texture
+    // assetProcessor.BakeTexture1d("Content/Texture/Engine/Gradiant 1d.png");
+}
+
+int
+main()
+{
+    AssetTool::Initialize();
+
+    BakeAssets();
+
+    return 0;
+}

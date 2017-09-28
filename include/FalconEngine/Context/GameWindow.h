6955efd74a23b7e2918e01b7a969b4d2ab8e6695
Wuxiang Lin <linwx_xin@hotmail.com>
2017-09-28 23:56:09 +0800

    Minor change to Qt window.

--- include/FalconEngine/Context/GameWindow.h
+++ include/FalconEngine/Context/GameWindow.h
@@ -8,6 +8,8 @@ namespace FalconEngine
 // @summary Game window represent the window system encapsulation. You would use
 // same GameWindow class for different window system like, GLFW, Qt etc.
 #if defined(FALCON_ENGINE_WINDOW_QT)
+#include <QtGui/QOpenGLFunctions>
+#include <QtWidgets/QOpenGLWidget>
 class FALCON_ENGINE_API GameWindow : public QOpenGLWidget, protected QOpenGLFunctions
 #elif defined(FALCON_ENGINE_WINDOW_GLFW)
 class FALCON_ENGINE_API GameWindow
